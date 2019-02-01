f3707a9f0e39e872f245d0577ffe06ae4d35d9fc
Jeffrey T Gosselin <jgoz.email@gmail.com>
2019-02-01 14:06:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,14 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
+let the_button = querySelector('a');
+the_button.addEventListener("click", appendQuestion)
+// Math.floor(Math.random() * 3) + 1;
 let question;
+let q = document.querySelector('div.question-container');
+
+function appendQuestion() {
+  h4 = document.createElement('h4');
+  h4.innerHTML = questions[number].questionText;
+  q.append(h4);
+}
